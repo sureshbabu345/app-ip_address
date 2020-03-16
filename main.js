97364215a80e989b4a9b0b3a4d33aa9e9d9acbb1
sureshbabu345 <usureshbabu7777@gmail.com>
2020-03-16 13:22:18 +0000

    Lab 3

--- main.js
+++ main.js
@@ -83,7 +83,6 @@ class IpAddress {
     // For more information, consult the Log Class guide on the Itential
     // Developer Hub https://developer.itential.io/ located
     // under Documentation -> Developer Guides -> Log Class Guide
-    log.info('Starting the IpAddress product.');
   }
 
   getFirstIpAddress(cidrStr, callback) {
